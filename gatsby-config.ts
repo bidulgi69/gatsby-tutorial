0c8e10e887d765125b4736d7ea825777e2f8e628
bidulgi69 <rhyme884@gmail.com>
2022-04-08 18:13:28 +0900

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,11 @@
+import type { GatsbyConfig } from "gatsby"
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `dove-demo`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
+
+export default config
